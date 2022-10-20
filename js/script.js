00f3a52984687dfd6e89ef130283e9eab35efb53
chiaramery <meringolachiara@gmail.com>
2022-10-20 14:24:51 +0200

    Miglioramenti

--- js/script.js
+++ js/script.js
@@ -11,7 +11,12 @@ const arrayRndNumberLenght = 5;
 // Creare array numeri generati random
 const arrayRndNumber = [];
 
-// Creare funzione che genera 5 numeri casuali non uguali da 1 a 100
+/**
+ * Description: funzione che genera 5 numeri casuali non uguali da 1 a 100
+ * @param {number} min -> numero minimo
+ * @param {number} max -> numero massimo
+ * @returns {number} random number
+ */
 function generetorRndNuber (min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
@@ -25,48 +30,72 @@ while(arrayRndNumber.length < arrayRndNumberLenght) {
     }
     i++;
 }
-
 // Array aggiunto al DOM
 const stampFirstArray = document.querySelector(".first-array");
 stampFirstArray.innerHTML = arrayRndNumber; 
 console.log(arrayRndNumber);
 
-
-// Settare timeOut dopo 30 secondi dalla comparsa dei numeri generati per far sparire i numeri
+/**
+ * Description: Settare timeOut dopo 30 secondi dalla comparsa dei numeri generati per far sparire i numeri
+ * @returns {object} array iniziale che scopare dopo 3 secondi
+ */
 function hidden () {
     stampFirstArray.classList.add("hidden");
 }
-setTimeout(hidden, 5000);
+setTimeout(hidden, 3000);
 
 // Creare array di numeri risposta dell'utente
 const userNumber = [];
 
 
-// Funzione che chiede all'utente tramite prompt i numeri visualizzati precedentemente 
-// Aggiungere numeri risposta dell'utente all'array
+/**
+ * Description: Funzione che chiede all'utente tramite prompt i numeri visualizzati precedentemente 
+ * @returns {number} numero che inserisce l'utente
+ */
 function number () {
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < arrayRndNumberLenght; i++){
         let userNumbers = parseInt(prompt("Inserisci i numeri memorizzati"));
-        userNumber.push(userNumbers);
+        if(!userNumber.includes(userNumbers)){
+            userNumber.push(userNumbers);
+        }
     }
     console.log(userNumber);
-    // Comparo i due array se sono uguali
-    let isSame = arrayRndNumber[i] == userNumber[i] && arrayRndNumber.every(function(element, index) {
-        if(userNumber.indexOf(element)>-1){
-          return element = userNumber[userNumber.indexOf(element)];
-        }
-      });
-      console.log(isSame);
+
+    // Verifico quali numeri ha indovinato l'utente
+    const guessedNumbers = findCommonElement (arrayRndNumber, userNumber);
+    console.log(guessedNumbers);
+
+    // Stampare output
+    printResult(parseInt(guessedNumbers));
 }
-setTimeout(number, 5000);
+setTimeout(number, 3000);
  
+/**
+ * Description: restituisce array con elementi in comune dei due array
+ * @param {array} firstArray primo array da confrontare
+ * @param {array} secondArray secondo array da confrontare
+ * @returns {array} array di numeri in comune
+ */
+function findCommonElement (firstArray, secondArray) {
+    const result = [];
+    // Per ogni elemento dell'array 
+    for (let i = 0; i < firstArray.length; i++) {
+        const thisElement = firstArray[i];
+        //    SE è incluso nel secondo array
+        if(secondArray.includes(thisElement)){
+            // Lo pusho nell'array finale
+            result.push(thisElement);
+        }
+    }
+    return result;
+}
 
-// SE numberRnd === userNumber 
-//    count++;
-//    pusho i numeri uguali nell' array soluzione;
-
-// Stampo array soluzioni nel DOM
-// const stampSecondArray = document.querySelector(".second-array");
-// stampSecondArray.innerHTML = arrayResult; 
-// console.log(arrayResult);
+/**
+ * Description: stampa numeri indovinati e la loro quantità
+ * @param {any} resultArray array di numeri da stampare
+ */
+function printResult(resultArray) {
+    const resultLast = document.getElementById("result");
+    resultLast.innerHTML +=`Hai indovinato ${resultArray.length} numeri: ${resultArray}`;
+}
 
